57ed0e3bb26a9f77a78ea6d3d79a4df7c30f2c58
Kona Ajay <konaajay0022@gmail.com>
2026-05-05 15:44:49 +0530

    Remove backend code, keep frontend only

--- sales-crm-main (1)/sales-crm-main/sales-crm-main/LeadManagement/README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-"# neww" 
-=======
-"# sales-backend-" 
->>>>>>> a43fd720be4cdbf99767cc6066a3bec98bcf0971
--- sales-crm-main (1)/sales-crm-main/sales-crm-main/LeadManagement/src/main/java/com/lms/www/leadmanagement/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.lms.www.leadmanagement.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
